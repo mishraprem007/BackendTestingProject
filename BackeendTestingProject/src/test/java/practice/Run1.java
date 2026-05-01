d22cba69df2a0cc3ca9042e4612d20f2cc64b63c
mishraprem007 <mishraprem007@gmail.com>
2026-05-01 16:47:02 +0530

    modifed comment

--- BackeendTestingProject/src/test/java/practice/Run1.java
+++ BackeendTestingProject/src/test/java/practice/Run1.java
@@ -14,7 +14,9 @@ public class Run1 {
 	@Test
 	public void sampleTest() {
 		/*
-		 * import com.backend.testing.A; import static com.backend.testing.A.*; ==>since we
+		 * import com.backend.testing.A; 
+		 * import static com.backend.testing.A.*; 
+		 * ==>since we
 		 * added static and .* in import, we don't have to use className to call methods
 		 * from A class
 		 */
