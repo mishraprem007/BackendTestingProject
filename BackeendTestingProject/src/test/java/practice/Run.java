8c2446dd0423723dacf141bfccd7472ea8f886fc
mishraprem007 <mishraprem007@gmail.com>
2026-05-01 16:44:36 +0530

    added one Run class

---
+++ BackeendTestingProject/src/test/java/practice/Run.java
@@ -0,0 +1,16 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+import com.backend.testing.A;
+
+
+
+public class Run {
+	@Test
+	public void sampleTest() {
+		A.given();
+		A.when();
+		A.then();
+	}
+}
